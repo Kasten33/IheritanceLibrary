a56c549d7359b16ffee393f4e86f5ec8179b8853
Kasten33 <hurlrykaden@gmail.com>
2025-02-14 16:33:38 -0700

    Add project files.

---
+++ IheritanceLibrary/Library.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace InheritanceLibrary;
+
+public class Program
+{
+    static void Main()
+    {
+
+        Book book = new ();
+        DVD dVD = new ();
+        Magazine magazine = new ();
+        book.inquery();
+        dVD.inquery2 ();    
+        magazine.inquer3 ();
+    }
+}
+
+
+
+public class LibraryItem
+{
+    private string Title = "Stirng";
+    private string Author = "St";
+
+    public void Callback()
+    {
+        Console.WriteLine(Title);
+        Console.WriteLine(Author);
+    }
+}
+class Book : LibraryItem
+{
+    private string PageCount = "100";
+    public void inquery()
+    {
+        Callback();
+        Console.WriteLine(PageCount);
+    }
+}
+class DVD : LibraryItem
+{
+    private string Duration = "1000";
+
+    public void inquery2()
+    {
+        Callback();
+        Console.WriteLine(Duration);
+    }
+}
+class Magazine : LibraryItem
+{
+    private int IssueNumber = 1;
+
+    public void inquer3()
+    {
+        Callback();
+        Console.WriteLine(IssueNumber);
+    }
+}
+
+
